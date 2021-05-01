8470cf69fd3b720de0d97276466dd12fcf89e0c2
reactionaryzebra <reactionaryzebra@gmail.com>
2021-04-30 18:59:07 -0700

    Create basic Team class

---
+++ models/Team.php
@@ -0,0 +1,32 @@
+<?php
+class Team{
+
+  public $name;
+  public $wins;
+  public $losses;
+  public $draws;
+  public $points;
+
+  public function __construct($name) {
+    $this->team = $name;
+  }
+
+  public function addResult($outcome) {
+    switch ($outcome) {
+      case 'win':
+        $this->wins = $this->wins + 1;
+        $this->points = $this->points + 3;
+        break;
+      case 'loss':
+        $this->losses = $this->losses + 1;
+        break;
+      case 'draw':
+        $this->draws = $this->draws + 1;
+        $this->points = $this->points + 1;
+        break;
+      default:
+        break;
+    }
+  }
+}
+ ?>
