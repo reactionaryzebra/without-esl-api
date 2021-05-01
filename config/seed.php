b3e394a2cbd6323d55fbc99c2700f0750e375584
reactionaryzebra <reactionaryzebra@gmail.com>
2021-04-30 18:17:56 -0700

    Finish seed file

--- config/seed.php
+++ config/seed.php
@@ -5,28 +5,35 @@ include_once '../models/result.php';
 $database = new Database();
 $db = $database->getConnection();
 
-// $create_table_query = "CREATE TABLE IF NOT EXISTS results (
-//   id serial NOT NULL PRIMARY KEY,
-//   season int NOT NULL,
-//   home_team varchar NOT NULL,
-//   away_team varchar NOT NULL,
-//   home_team_goals int NOT NULL,
-//   away_team_goals int NOT NULL,
-//   outcome varchar(1) NOT NULL
-// );";
-// $table = pg_query($db, $create_table_query);
+$create_table_query = "
+DROP TABLE IF EXISTS results;
+CREATE TABLE IF NOT EXISTS results (
+  id serial NOT NULL PRIMARY KEY,
+  season int NOT NULL,
+  home_team varchar NOT NULL,
+  away_team varchar NOT NULL,
+  home_team_goals int NOT NULL,
+  away_team_goals int NOT NULL,
+  outcome varchar(1) NOT NULL
+);";
+$table = pg_query($db, $create_table_query);
 
-$data_1011 = file_get_contents('../data/season-1011_json.json');
-$results_1011 = json_decode($data_1011, true);
+$all_files = new DirectoryIterator('../data/');
 
-foreach ($results_1011 as $_result) {
-  $result = new Result($db);
-  $result->season = "1011";
-  $result->home_team = $_result['HomeTeam'];
-  $result->away_team = $_result['AwayTeam'];
-  $result->home_team_goals = $_result['FTHG'];
-  $result->away_team_goals = $_result['FTAG'];
-  $result->outcome = $_result['FTR'];
-  $success = $result->create();
+foreach ($all_files as $file) {
+  if ($file->isDot()) continue;
+  $season = explode('.', explode('-', $file->getPathname())[1])[0];
+  $data = file_get_contents($file->getPathname());
+  $results = json_decode($data);
+  foreach ($results as $_result) {
+    $result = new Result($db);
+    $result->season = $season;
+    $result->home_team = $_result->HomeTeam;
+    $result->away_team = $_result->AwayTeam;
+    $result->home_team_goals = $_result->FTHG;
+    $result->away_team_goals = $_result->FTAG;
+    $result->outcome = $_result->FTR;
+    $success = $result->create();
+  }
 }
  ?>
